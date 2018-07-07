7bef6c6524e2725b8ce30e9c3e081c44e58ac1b2
Riko Pernando <rikopernando96@gmail.com>
2018-07-07 09:28:12 +0700

    tambahkan length di migration
    tambahkan length di migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->increments('id',100);
+            $table->string('name',100);
+            $table->string('email',100)->unique();
+            $table->string('password',100);
             $table->rememberToken();
             $table->timestamps();
         });
