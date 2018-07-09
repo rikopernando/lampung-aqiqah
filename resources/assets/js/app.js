7c413bf950e6e4bf0add1b93d237aa0756288bf2
Riko Pernando <rikopernando96@gmail.com>
2018-07-09 14:56:29 +0700

    buat component list produk
    buat component list produk

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,17 +4,14 @@ window.Vue = require('vue');
 
 import routes from './router'
 import VueRouter from 'vue-router'
-import { MdButton, MdContent, MdTabs, MdIcon } from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css' // This line here
 
+
 window.Vue.use(VueRouter)
 
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
-Vue.use(MdIcon)
-
+Vue.use(VueMaterial)
 
 const router = new VueRouter({ routes })
 
--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,10 +1,15 @@
 import Example from '../components/Example'
+import PaketAqiqah from '../components/home/produk'
 
 const routes = [
         {
           path : '/',
           name : 'home',
           component : Example
+        },{
+          path : '/paket',
+          name : 'paket',
+          component : PaketAqiqah
         }
     ]
 
