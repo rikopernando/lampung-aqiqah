203a9767e224ea9815d8f0b2be6d0a3982d0679b
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-07-10 14:00:24 +0700

    Perbaikan Slider dan layout kelebihan

--- resources/assets/js/components/home/slider.vue
+++ resources/assets/js/components/home/slider.vue
@@ -1,29 +1,100 @@
+<style lang="scss" scoped>
+
+.md-toolbar.md-theme-default {
+     background-color:#d21d24;
+  }
+  .md-toolbar.md-theme-default .md-title, .md-toolbar.md-theme-default .md-display-1, .md-toolbar.md-theme-default .md-display-2 {
+    color: white;
+    font-size: 25px;
+    font-family: "Roboto", Sans-serif;
+    font-weight: 600;
+    align-items:center;
+}
+  .md-content {
+    width: 200px;
+    height: 200px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .md-layout-item {
+    height: 40px;
+
+    &:nth-child(1) {
+      background: md-get-palette-color(grey, 300);
+    }
+
+    &:nth-child(2) {
+      background: md-get-palette-color(grey, 400);
+    }
+
+    &:nth-child(3) {
+      background: md-get-palette-color(grey, 500);
+    }
+  }
+  .desc{
+      color: #7a7a7a;
+      font-family: "Roboto", Sans-serif;
+      font-weight: 400;
+  }
+  .headDesc{
+      color: #d21d24;
+      font-size: 25px;
+      font-weight: 400;
+  }
+
+</style>
+
 <template>
-    <vueper-slides autoplay :infinite="false" :bullets-outside="true">
+  <div>
+  <div >
+    <vueper-slides fade slide-content-outside="top" slide-content-outside-class="max-widthed" :touchable="false" :slide-ratio="0.3">
       <vueper-slide 
           v-for="(slide, i) in slides" 
           :key="i" 
           :image="slide.image">
       </vueper-slide>
     </vueper-slides>
+    <md-toolbar class="md-dense">
+      <center><h3 class="md-title" >MENGAPA MEMILIH AQIQAH LAMPUNG?</h3></center>
+    </md-toolbar>
+    </div>
+   <div class="md-layout">
+    <div class="md-layout-item">
+
+    </div>
+    <div class="md-layout-item">  
+        <md-icon class="md-size-2x fa fa-local_shipping"></md-icon><h3 class="headDesc"><b>Free Ongkir</b></h3>
+      <p class="desc">Kami memberikan bebas biaya Pengiriman untuk wilayah Bandar Lampung dan sekitarnya dengan radius maksimal 30 KM</p>
+    </div>
+     <div class="md-layout-item">  
+
+    </div>
+     <div class="md-layout-item">  
+    </div>
+    <div class="md-layout-item">
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
   import { VueperSlides, VueperSlide } from 'vueperslides'
   export default {
     components: { VueperSlides, VueperSlide },
+
     data : () => {
       return {
           slides : [
                {
                   title: 'Slider 1',
                   content: 'Slide Content',
-                  image : 'https://aqiqahlampung.co.id/wp-content/uploads/2018/04/menu5.png'
+                  image : 'http://aqiqahlampung.co.id/wp-content/uploads/slider2/banner-web-crop.jpeg'
                },
                {
                   title: 'Slider 1',
                   content: 'Slide Content',
-                  image: 'https://aqiqahlampung.co.id/wp-content/uploads/2018/04/menu4.png'
+                  image: 'https://aqiqahlampung.co.id/wp-content/uploads/2018/04/banner-sate-crop.jpg'
                },
                {
                   title: 'Slider 1',
@@ -32,33 +103,10 @@
                }
            ]
       }
+
    },
    mounted() {
      console.log(this.slides)
    }
   }
 </script>
-
-<style lang="scss" scoped>
-.vueperslides__bullet {
-  background-color: rgba(0, 0, 0, 0.3);
-  border: none;
-  box-shadow: none;
-  transition: 0.3s;
-  width: 16px;
-  height: 16px;
-}
-
-.vueperslides__bullet--active {
-  background-color: #ff5252;
-}
-
-.vueperslides__bullet span {
-  display: block;
-  color: #fff;
-  font-size: 10px;
-  opacity: 0.8;
-}
-
-</style>
-
