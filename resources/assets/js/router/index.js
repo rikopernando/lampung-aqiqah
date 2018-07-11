6c7993a8779cbcddc483395a9c6785dbdded22f5
Muhammad Hari <Nrmuhamad@gmail.com>
2018-07-11 15:00:59 +0700

    Footer tampilan pc dan hp

--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,10 +1,16 @@
 import Example from '../components/Example'
+import Footer from '../components/Footer/Footer'
 
 const routes = [
         {
           path : '/',
           name : 'home',
           component : Example
+        },
+        {
+          path : '/footer',
+          name : 'footer',
+          component : Footer
         }
     ]
 
