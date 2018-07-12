f98f27f7a6642aac62b6e3c6374d9a0b4891412e
Riko Pernando <rikopernando96@gmail.com>
2018-07-12 16:50:37 +0700

    buat component login
    buat component login

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
        <md-button class="md-raised md-primary">Primary</md-button>
+       <register></register>
+       <login></login>
     </div>
 </template>
 
 <script>
     import Register from './auth/register'
+    import Login from './auth/login'
     export default {
         components : {
-          Register
+          Register,Login
         },
         mounted() {
             console.log('Component mounted.')
--- resources/assets/js/components/auth/login.vue
+++ resources/assets/js/components/auth/login.vue
@@ -1,40 +1,28 @@
 <template>
   <div class="container">
-   <form novalidate class="md-layout" @submit.prevent="saveRegister">
+   <form novalidate class="md-layout" @submit.prevent="saveLogin">
       <md-card class="md-layout-item">
         <md-card-header>
-          <div class="md-title">Register</div>
+          <div class="md-title">Login</div>
         </md-card-header>
 
         <md-card-content>
             
 						<md-field :class="messageClass">
-              <md-icon>perm_identity</md-icon>
-							<label>Nama</label>
-							<md-input v-model="register.name" type="text"></md-input>
-              <span v-if="errors.name" class="md-error">{{ errors.name[0] }}</span>
-						</md-field>
-						<md-field :class="messageClass">
               <md-icon>email</md-icon>
 							<label>Email</label>
-							<md-input v-model="register.email" type="email"></md-input>
+							<md-input v-model="login.email" type="email"></md-input>
               <span v-if="errors.email" class="md-error">{{ errors.email[0] }}</span>
 						</md-field>
 						<md-field :class="messageClass">
               <md-icon>lock</md-icon>
 							<label>Password</label>
-							<md-input v-model="register.password" type="password"></md-input>
+							<md-input v-model="login.password" type="password"></md-input>
               <span v-if="errors.password" class="md-error">{{ errors.password[0] }}</span>
-						</md-field>
-						<md-field :class="messageClass">
-              <md-icon>lock</md-icon>
-							<label>Konfirmasi Password</label>
-							<md-input v-model="register.password_confirmation" type="password"></md-input>
 						</md-field>
 
             <md-progress-bar md-mode="indeterminate" v-if="loading"></md-progress-bar>
-            <md-snackbar :md-active.sync="snackbar">Registrasi Berhasil!</md-snackbar>
-            <center><md-button class="btn" type="submit" :disabled="loading">Registrasi</md-button></center>
+            <center><md-button class="btn" type="submit" :disabled="loading">Login</md-button></center>
 
 				</md-card-content>
       </md-card>
@@ -47,15 +35,12 @@
 		data: () => ({
       errors : {},
       url : window.location.origin + window.location.pathname,
-			register : {
-				name : '',
+			login : {
         email : '',
         password : '',
-        password_confirmation : ''
 			},
       validation : false,
       loading : false,
-      snackbar : false
 		}),
     computed : {
       messageClass () {
@@ -65,14 +50,13 @@
       }
     },
     methods : {
-      saveRegister() {
+      saveLogin() {
         const app = this
         app.loading = true
-        axios.post(app.url+'register', app.register)
+        axios.post(app.url+'login', app.login)
         .then((resp) => {
           console.log(resp)
           app.loading = false
-          app.snackbar = true
           app.clearForm()
         })
         .catch((err) => {
@@ -84,10 +68,8 @@
       },
       clearForm() {
         const app = this
-        app.register.name = ""
         app.register.email = ""
         app.register.password = ""
-        app.register.password_confirmation = ""
       }
     }
   }
