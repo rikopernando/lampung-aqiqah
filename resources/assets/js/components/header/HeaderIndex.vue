0561019a8e48e0eeb722cb2ec347d2e1a50d5867
RindangRamadhan <rindangramadhan10@gmail.com>
2018-07-14 09:47:36 +0700

    Perbaiki responsive header dekstop

--- resources/assets/js/components/header/HeaderIndex.vue
+++ resources/assets/js/components/header/HeaderIndex.vue
@@ -1,112 +1,74 @@
 <template>
-  <div class="md-layout md-gutter">
+  <div>
+    <md-toolbar md-elevation="0" style="background-color: #da2921; min-height:10px">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="../resources/assets/js/components/header/HeaderStyle.css">
 
-        <div class="md-layout-item md-medium-hide">
-          <div class="md-tabs-navigation header-top">
-            <div class="menu-kanan">
-              <a href="#/pemesanan" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav">Pemesanan</div>
-                </div>
-              </a>
-              <a href="#/pembayaran" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav">Pembayaran</div>
-                </div>
-              </a>
-              <a href="#/keagenan" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav">Keagenan</div>
-                </div>
-              </a>
-              <a href="#/informasi" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav">Informasi</div>
-                </div>
-              </a>
-              <a href="#/kritik-saran" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav">Ruang Saran dan Pengaduan</div>
-                </div>
-              </a>
+      <h3 class="md-title" style="flex: 1"></h3>
+      <md-button href="#/pemesanan" style="color: white; font-size: 12px">Pemesanan</md-button>
+      <md-button href="#/pembayaran" style="color: white; font-size: 12px">Pembayaran</md-button>
+      <md-button href="#/keagenan" style="color: white; font-size: 12px">Keagenan</md-button>
+      <md-button href="#/informasi" style="color: white; font-size: 12px">Informasi</md-button>
+      <md-button href="#/kritik-saran" style="color: white; font-size: 12px">Ruang Saran dan Pengaduan</md-button>
+      <md-button class="md-icon-button">
+        <a href="#/facebook">
+          <i class="fa fa-facebook-f icon-link"></i>
+          <md-tooltip md-direction="bottom">Follow On Facebook </md-tooltip>
+        </a>
+      </md-button>
+      <md-button class="md-icon-button">
+        <a href="#/instagram">
+          <i class="fa fa-instagram icon-link"></i>
+          <md-tooltip md-direction="bottom">Follow On Instagram </md-tooltip>
+        </a>
+      </md-button>
+      <md-button class="md-icon-button">
+        <a href="#/whatsapp">
+          <i class="fa fa-whatsapp icon-link"></i>
+          <md-tooltip md-direction="bottom">Send us Whatsapp </md-tooltip>
+        </a>
+      </md-button>
+    </md-toolbar>
 
-              <a href="#/facebook">
-                <i class="fa fa-facebook-f icon-link"></i>
-                <md-tooltip md-direction="bottom">Follow On Facebook </md-tooltip>
-              </a>
-              <a href="#/instagram">
-                <i class="fa fa-instagram icon-link"></i>
-                <md-tooltip md-direction="bottom">Follow On Instagram </md-tooltip>
-              </a>
-              <a href="#/whatsapp">
-                <i class="fa fa-whatsapp icon-link"></i>
-                <md-tooltip md-direction="bottom">Send us Whatsapp </md-tooltip>
-              </a>
+    <md-toolbar class="md-dense" md-elevation="0" style="background-color: white">
+      <div class="md-toolbar-row">
+          <a href="#/" class="icon-aqiqah md-button md-theme-default md-active">
+              <img src="https://rumahaqiqah.org/wp-content/uploads/2018/01/rumahaqiqah3.png" alt="Aqiqah Lampung">
+          </a>
+
+        <div class="md-toolbar-section-start">
+          <div style="padding-left: 50px">
+            <i class="fa fa-search icon-box"></i>
+            <input type="text" placeholder="Search..." class="input-box">
+          </div>
+          <a href="#/" class="md-button md-theme-default md-active">
+            <div class="md-ripple">
+              <div class="label-nav-main">HOME</div>
             </div>
-          </div>
-
-          <md-toolbar class="header-main">
-              <a href="#/" class="icon-aqiqah md-button md-theme-default md-active">
-                  <img src="https://rumahaqiqah.org/wp-content/uploads/2018/01/rumahaqiqah3.png" alt="Aqiqah Lampung">
-              </a>
-
-              <div style="padding-left: 50px">
-                <i class="fa fa-search icon-box"></i>
-                <input type="text" placeholder="Search..." class="input-box">
-              </div>
-              <a href="#/" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav-main">HOME</div>
-                </div>
-              </a>
-              <a href="#/produk-list" class="md-button md-theme-default md-active">
-                <div class="md-ripple">
-                  <div class="label-nav-main">PRODUK</div>
-                </div>
-              </a>
-
-              <div class="menu-kanan">
-                <a href="#/login" class="md-button md-theme-default md-active">
-                  <div class="md-ripple">
-                    <div class="label-nav-main">LOGIN</div>
-                  </div>
-                </a>
-                <a href="#/cart" class="md-button md-theme-default md-active">
-                  <div class="md-ripple">
-                    <div class="label-nav-main">CART/Rp. 0</div>
-                    <i class="fa fa-shopping-basket icon-shop"></i>
-
-                  </div>
-                </a>
-              </div>
-          </md-toolbar>
+          </a>
+          <a href="#/produk-list" class="md-button md-theme-default md-active">
+            <div class="md-ripple">
+              <div class="label-nav-main">PRODUK</div>
+            </div>
+          </a>
         </div>
 
-        <div id="displaySidenav">
-          <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-          </div>
-
-          <div class="container header-top" style="height: 30px"> </div>
-
-          <md-toolbar class="header-main">
-            <span style="font-size:30px;cursor:pointer;color: #868686;" @click="openNav()">&#9776;</span>
-
-            <a href="#/" class="icon-aqiqah-mobile">
-                <img src="https://rumahaqiqah.org/wp-content/uploads/2018/01/rumahaqiqah3.png" alt="Aqiqah Lampung">
-            </a>
-
-            <i class="fa fa-shopping-basket icon-shop-mobile"></i>
-          </md-toolbar>
-
+        <div class="md-toolbar-section-end">
+          <a href="#/login" class="md-button md-theme-default md-active">
+            <div class="md-ripple">
+              <div class="label-nav-main">LOGIN</div>
+            </div>
+          </a>
+          <a href="#/cart" class="md-button md-theme-default md-active">
+            <div class="md-ripple">
+              <div class="label-nav-main">CART/Rp. 0</div>
+              <i class="fa fa-shopping-basket icon-shop"></i>
+            </div>
+          </a>
         </div>
 
+      </div>
+    </md-toolbar>
   </div>
 </template>
 
