f2613a800b381af1948150e4df12d944f0048050
Muhammad Hari <Nrmuhamad@gmail.com>
2018-07-16 14:10:57 +0700

    halaman edit user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        return response(User::whereId($id)->first());
     }
 
     /**
---
+++ resources/assets/js/components/User/UserEdit.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form>
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Users</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field>
+            <label for="first-name">First Name</label>
+            <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="user.name" />
+          </md-field>
+          <md-field>
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="user.email" />
+          </md-field>
+        </md-card-content>
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Edit User</md-button>
+        </md-card-actions>
+      </md-card>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    url: window.location.origin + (window.location.pathname + 'user/'),
+    user: {}
+  }),
+  mounted() {
+    this.getDataUser(this.$route.params.id);
+  },
+  methods: {
+    getDataUser(userId) {
+      axios.get(this.url + userId)
+      .then(resp => {
+        this.user = resp.data;
+      })
+      .catch(resp => {
+        console.log('catch:', resp);
+      });
+    }
+  }
+}  
+</script>
