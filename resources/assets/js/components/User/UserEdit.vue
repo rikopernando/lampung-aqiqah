a9a9ac58fd30bd528437361ff4c524827d181e09
Muhammad Hari <Nrmuhamad@gmail.com>
2018-07-18 09:19:12 +0700

    Memvisualisasikan loading di halaman edit user
    - Menambahkan spinner loading
    - Membuat label dari nama dan email tidak tampil selama loading (akan tampil ketika data sudah didapat)
    - Button form ketika diklik akan berubah menjadi spinner kecil (sebagai penegas bahwa data sedang di proses) dan akan berubah menjadi button lagi setelah proses selesai

--- resources/assets/js/components/User/UserEdit.vue
+++ resources/assets/js/components/User/UserEdit.vue
@@ -7,17 +7,23 @@
         </md-card-header>
 
         <md-card-content>
+          <div v-if="loading" class="_spinner-container">
+            <div class="_spinner">
+              <md-progress-spinner :md-diameter="80" :md-stroke="5" md-mode="indeterminate"></md-progress-spinner>
+            </div>
+          </div>
           <md-field>
-            <label for="name">Nama</label>
+            <label v-if="!loading" for="name">Nama</label>
             <md-input name="name" id="name" v-model="user.name" />
           </md-field>
           <md-field>
-            <label for="email">Email</label>
+            <label v-if="!loading" for="email">Email</label>
             <md-input type="email" name="email" id="email" v-model="user.email" />
           </md-field>
         </md-card-content>
         <md-card-actions>
-          <md-button type="submit" class="md-primary">Edit User</md-button>
+          <md-progress-spinner v-if="submitted" :md-diameter="30" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
+          <md-button v-else type="submit" class="md-primary">Edit User</md-button>
         </md-card-actions>
       </md-card>
     </form>
@@ -36,7 +42,9 @@ export default {
       name: '',
       email: ''
     },
-    snackbarEditUser: false
+    snackbarEditUser: false,
+    submitted: false,
+    loading: true
   }),
   mounted() {
     this.getDataUser(this.$route.params.id);
@@ -46,15 +54,18 @@ export default {
       axios.get(this.url + userId)
       .then(resp => {
         this.user = resp.data;
+        this.loading = false;
       })
       .catch(resp => {
         console.log('catch getDataUser:', resp);
       });
     },
     saveForm() {
+      this.submitted = true;
       axios.patch(this.url + this.$route.params.id, this.user)
       .then(resp => {
         this.snackbarEditUser = true;
+        this.submitted = false;
       })
       .catch(resp => {
         console.log('catch saveForm:', resp);
@@ -66,3 +77,14 @@ export default {
   }
 }  
 </script>
+
+<style>
+  ._spinner-container {
+    position: absolute; 
+    left: 50%;
+  }
+  ._spinner {
+    position: relative; 
+    left: -50%;
+  }
+</style>
