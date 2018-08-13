c4fce8ea6804a7c263980d5866058430d6c8b404
RindangRamadhan <rindangramadhan10@gmail.com>
2018-08-13 10:03:10 +0700

    Download Katalog Produk Lampung Aqiqah

--- resources/assets/js/components/footer/footer.vue
+++ resources/assets/js/components/footer/footer.vue
@@ -7,21 +7,27 @@
 	        <span class="md-title">Download Katalog</span>
 	    	</div>
 	      <div>
-      		<md-field>
-			      <label>Nama</label>
-			      <md-input></md-input>
-			    </md-field>
-      		<md-field>
-			      <label>Email</label>
-			      <md-input></md-input>
-			    </md-field>
-      		<md-field>
-			      <label>Telpon</label>
-			      <md-input></md-input>
-			    </md-field>
-			    <div style="text-align: center;">
-				    <md-button type="submit" class="md-primary">Download</md-button>
-			    </div>
+          <form novalidate v-on:submit.prevent="validateKatalog">
+            <md-field :class="getValidationClass('nama')">
+              <label for="nama">Nama Lengkap</label>
+              <md-input name="nama" id="nama" v-model="dataKatalog.nama" ref="nama"/>
+              <span class="md-error" v-if="!$v.dataKatalog.nama.required">{{error.nama}}</span>
+            </md-field>
+            <md-field :class="getValidationClass('email')">
+              <label for="email">Email</label>
+              <md-input name="email" id="email" v-model="dataKatalog.email" ref="email"/>
+              <span class="md-error" v-if="!$v.dataKatalog.email.required">{{error.email}}</span>
+              <span class="md-error" v-else-if="!$v.dataKatalog.email.email">Format Email Salah !</span>
+            </md-field>
+            <md-field :class="getValidationClass('telpon')">
+              <label for="telpon">No. Telpon</label>
+              <md-input name="telpon" id="telpon" v-model="dataKatalog.telpon" ref="telpon"/>
+              <span class="md-error" v-if="!$v.dataKatalog.telpon.required">{{error.telpon}}</span>
+            </md-field>
+            <div style="text-align: center;">
+  				    <md-button type="submit" class="md-primary">Download</md-button>
+  			    </div>
+          </form>
 				</div>
 	    </div>
 
@@ -51,11 +57,63 @@
 </div>
 </template>
 <script >
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+  import { validationMixin } from 'vuelidate'
+  import { required, email } from 'vuelidate/lib/validators'
+  export default {
+    mixins: [validationMixin],
+    data: () => ({
+      urlKatalog: 'https://gdurl.com/f4i3/download',
+      error: {
+        nama: '',
+        email: '',
+        telpon: ''
+      },
+      dataKatalog: {
+        nama: '',
+        email: '',
+        telpon: ''
+      }
+    }),
+    validations: {
+      dataKatalog: {
+        nama: {
+          required,
+        },
+        email: {
+          required,
+          email
+        },
+        telpon: {
+          required,
         }
+      }
+    },
+    methods: {
+      getValidationClass (fieldName) {
+        const field = this.$v.dataKatalog[fieldName]
+
+        if (field) {
+          this.error.nama = "Nama Lengkap Harus Diisi !";
+          this.error.email = "Email Harus Diisi !";
+          this.error.telpon = "No Telpon Harus Diisi !";
+          return {
+            'md-invalid': field.$invalid && field.$dirty
+          }
+        }
+      },
+      validateKatalog() {
+        this.$v.$touch()
+
+        if (!this.$v.$invalid) {
+          this.downloadKatalog()
+        }
+      },
+
+      downloadKatalog() {
+        window.open(this.urlKatalog, '_blank')
+      }
     }
+  }
 </script>
 <style scoped>
   .row {
@@ -75,5 +133,4 @@
   .head {
   	margin-bottom: 10px;
   }
-
 </style>
