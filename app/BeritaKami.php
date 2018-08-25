896d7f12b8870ecf0fd1ce50054820c1fd670de5
fahrizalrahman <fahrizal95rhm@gmail.com>
2018-08-25 09:36:24 +0700

    buat model, migration dan controller

---
+++ app/BeritaKami.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BeritaKami extends Model
+{
+    //
+    protected $fillable = ['judul_berita','isi_berita','foto'];
+}
---
+++ database/migrations/2018_08_24_161429_create_berita_kamis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBeritaKamisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita_kamis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul_berita');
+            $table->string('isi_berita');
+            $table->string('foto')->nullable();
+            $table->unsignedInteger('created_by')->nullable()->index();
+            $table->unsignedInteger('updated_by')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita_kamis');
+    }
+}
