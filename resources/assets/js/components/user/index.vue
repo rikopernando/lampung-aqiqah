f44a17ee32357bf9b34ad9e1e9494f6795c021dd
Muhammad Hari <Nrmuhamad@gmail.com>
2018-08-31 08:56:43 +0700

    Mengubah link home menjadi dashboard di user

--- resources/assets/js/components/user/index.vue
+++ resources/assets/js/components/user/index.vue
@@ -66,7 +66,7 @@
 
      	<md-card>
       	<ul class="breadcrumb">
-        	<li><router-link :to="{name: 'home'}">Home</router-link></li>
+        	<li><router-link :to="{name: 'dashboard'}">Dashboard</router-link></li>
         	<li class="active">User</li>
       	</ul>
       </md-card>
