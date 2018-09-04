9c1c6623804987a1fa9d4985513725107ed29573
Riko Pernando <rikopernando96@gmail.com>
2018-09-04 11:38:19 +0700

    create custome errors page
    create custome errors page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::layouts')
+
+@section('title','no access')
+
+@section('message','Maaf, anda tidak memiliki akses untuk fitur ini')
+
+
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::layout')
+
+@section('title', 'Halaman Tidak Di temukan')
+
+@section('message', 'Maaf , halaman yang anda cari tidak di temukan.')
+
