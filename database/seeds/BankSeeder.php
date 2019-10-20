90f0248ece44b15353787cb76229f3b74ab5d9a3
Rindang Ramadhan <r.ramadhan@decode.com.sa>
2019-10-20 11:49:59 +0700

    add new seeders bank

--- database/seeds/BankSeeder.php
+++ database/seeds/BankSeeder.php
@@ -18,5 +18,12 @@ class BankSeeder extends Seeder
       $mitra->no_rek = "214515815";
       $mitra->default = "1";
       $mitra->save();
+      
+      $mitra = new Bank();
+      $mitra->nama_bank = "BCA Syariah";
+      $mitra->atas_nama = "Aqiqah Lampung";
+      $mitra->no_rek = "101023989328";
+      $mitra->default = "0";
+      $mitra->save();
     }
 }
